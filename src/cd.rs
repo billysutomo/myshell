d165bdf4903d0112aa661a666bfb42ac91e49071
Billy Sutomo <billysutomo.53@gmail.com>
2023-08-06 13:34:40 +0700

    add more options

--- src/cd.rs
+++ src/cd.rs
@@ -8,19 +8,16 @@ use std::{
 };
 
 pub fn change_directory(args: SplitWhitespace) -> Result<(), io::Error> {
-    let (first, second) = extract_command(args);
+    let (command, param) = extract_command(args);
 
-    println!("first {first}");
-    println!("second {second}");
-    if second != "-" {
+    if command != "-" {
         env::set_var("OLDPWD", env::current_dir().unwrap());
     }
 
-    match first.as_str() {
+    match command.as_str() {
         "~" => {
-            println!("pattern ~|{}", second);
-            if second != "" {
-                let full_path = home::home_dir().unwrap().join(second);
+            if param != "" {
+                let full_path = home::home_dir().unwrap().join(param);
                 let root = Path::new(&full_path);
                 return env::set_current_dir(&root);
             }
@@ -30,12 +27,10 @@ pub fn change_directory(args: SplitWhitespace) -> Result<(), io::Error> {
             env::set_current_dir(&root)
         }
         "-" => {
-            println!("pattern -|");
             let a = env::var("OLDPWD").unwrap();
             env::set_current_dir(a)
         }
         x => {
-            println!("pattern x|{x}");
             let root = Path::new(x);
             env::set_current_dir(&root)
         }
@@ -46,30 +41,42 @@ pub fn change_directory(args: SplitWhitespace) -> Result<(), io::Error> {
  * supported
  * 1. cd ~
  * 2. cd ~/Engineering or directory with relative path
- * 3.
- *
- * bug
- * 1. cd .
- * 2. cd ..
- * 3. cd - / or back to last history or path
+ * 3. cd . || current directory
+ * 4. cd .. || step back directory
+ * 5. cd - || or back to last history or path
  *
  */
 fn extract_command(args: SplitWhitespace) -> (String, String) {
-    let mut binding = args.peekable();
-    binding.peek();
-    let new_dir = binding.next().map_or("~", |x| x);
-    let mut a = String::default();
-    let mut b = new_dir.chars();
-    a.push(b.next().unwrap());
-    b.next();
+    let mut original_string: String = args.collect();
+    let two_char: String = original_string.clone().chars().take(2).collect();
 
-    return (a.to_string(), b.as_str().to_string());
-}
-
-fn extract_command_new(args: SplitWhitespace) -> (String, String) {
-    let sub = args.take(2).collect();
-
-    return (a.to_string(), b.as_str().to_string());
+    match two_char.as_str() {
+        ".." => {
+            return ("..".to_string(), "".to_string());
+        }
+        "~/" => {
+            original_string.remove(0);
+            original_string.remove(0);
+            return ("~".to_string(), original_string);
+        }
+        x => {
+            let b: String = x.chars().take(1).collect();
+            match b.as_str() {
+                "." => {
+                    return (".".to_string(), "".to_string());
+                }
+                "~" => {
+                    return ("~".to_string(), "".to_string());
+                }
+                "-" => {
+                    return ("-".to_string(), "".to_string());
+                }
+                _ => {
+                    return (original_string, "".to_string());
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -77,14 +84,32 @@ mod tests {
     use crate::cd::extract_command;
 
     #[test]
+    fn directory_path() {
+        let result = extract_command("directory".split_whitespace());
+        assert_eq!(result, ("directory".to_string(), "".to_string()));
+    }
+
+    #[test]
     fn relative_path() {
-        let result = extract_command("~/Engineering".split_whitespace());
-        assert_eq!(result, ("~".to_string(), "Engineering".to_string()));
+        let result = extract_command("~/directory".split_whitespace());
+        assert_eq!(result, ("~".to_string(), "directory".to_string()));
     }
 
     #[test]
     fn current_path() {
         let result = extract_command(".".split_whitespace());
         assert_eq!(result, (".".to_string(), "".to_string()));
+    }
+
+    #[test]
+    fn last_path() {
+        let result = extract_command("-".split_whitespace());
+        assert_eq!(result, ("-".to_string(), "".to_string()));
+    }
+
+    #[test]
+    fn back_path() {
+        let result = extract_command("..".split_whitespace());
+        assert_eq!(result, ("..".to_string(), "".to_string()));
     }
 }
